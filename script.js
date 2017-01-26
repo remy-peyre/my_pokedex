6278953865d954d8949659de726052f451dbafd0
remy peyre <remy.peyre05@gmail.com>
2017-01-26 23:11:05 +0100

    modif architecture

---
+++ script.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<script src="script.js"></script>
+		<link rel="stylesheet" href="style.css" type="text/css">
+		<title>My Pokedex</title>
+	</head>
+	<body>
+		
+	</body>
+</html>
