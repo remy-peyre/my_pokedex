70db81977a14ad1f11e9543d148e3ed47ccc2bb4
remy peyre <remy.peyre05@gmail.com>
2017-02-03 11:19:51 +0100

    show img name and type

--- script.js
+++ script.js
@@ -1,77 +1,48 @@
-/*function request(callback) {
+window.onload = function() {
 
-    var xhr = getXMLHttpRequest();
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+        if(req.readyState == 4 && req.status == 200){
+            pokemon = JSON.parse(req.responseText);
+        } else {}
+    };
+    req.open("get", "pokemons.json", true);
+    req.send();
 
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
-            callback(xhr.responseXML);
+    document.querySelector('#form').onclick = function(){
+
+        var name = document.querySelector('#showName');
+        var type = document.querySelector('#showType');
+        var image = document.querySelector('#img_zone');
+        var errorBlock = document.querySelector('#errorBlock');
+        var pokedexValid = false;
+
+        var infoPokemon = document.querySelector('#name').value;
+
+        for( var i in pokemon){
+            console.log(i);
+            if (pokemon[i].name == infoPokemon ){
+                pokedexValid = true;
+                errorBlock.innerHTML='';
+                name.innerHTML = pokemon[i].name;
+                type.innerHTML = pokemon[i].type;
+                image.innerHTML = '<img class="picture_size" src="http://img.pokemondb.net/artwork/'+pokemon[i].name.toLowerCase()+'.jpg">';
+            }
+            if ( infoPokemon == i){
+                pokedexValid = true;
+                errorBlock.innerHTML='';
+                console.log(pokemon);
+                name.innerHTML = pokemon[i].name;
+                type.innerHTML = pokemon[i].type;
+                image.innerHTML = '<img class="picture_size" src="http://img.pokemondb.net/artwork/'+pokemon[i].name.toLowerCase()+'.jpg">';
+            }
+
         }
-    };
+        return false ;
 
-    xhr.open("GET", "XMLHttpRequest_getXML.xml", true);
-    xhr.send(null);
-}
-
-function readData(oData) {
-    var nodes = oData.getElementsByTagName("soft");
-    var ol = document.createElement("ol"), li, cn;
-
-    for (var i=0, c=nodes.length; i<c; i++) {
-        li = document.createElement("li");
-        cn = document.createTextNode(nodes[i].getAttribute("name"));
-
-        li.appendChild(cn);
-        ol.appendChild(li);
     }
 
-    document.getElementById("output").appendChild(ol);
-}*/
 
 
 
-
-
-
-
-
-
-
-
-
-
-/*window.onload = function() {
-
-
-
-
-
-
-
-
-
-
-    /*var goal = document.querySelector('#goal');
-
-    goal.onclick = function(){
-        goal.style.background = '#000';
-        goal.style.color = '#fff';
-    };
-
-    var goGoal = document.querySelectorAll('.gogoal');
-
-    for(var i = 0; i < goGoal.length; i++){
-        goGoal[i].onclick = function(){
-            this.style.background = '#33CC33';
-            this.style.color = '#FFFF00';
-        };
-    }
-
-    var p_list = document.querySelectorAll('p');
-
-    for(var i = 0; i < p_list.length; i++){
-        p_list[i].onclick = function(){
-            this.parentNode.removeChild(this.nextSibling);
-        };
-
-
-};*/
+};
